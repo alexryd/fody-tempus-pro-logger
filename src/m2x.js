d3adcf788be4f4dbb237874c35d0db1681cf7c9a
Alexander Ryden <alexander.ryden@gmail.com>
2018-01-09 17:01:39 +0100

    Remove an unused function

--- src/m2x.js
+++ src/m2x.js
@@ -7,23 +7,6 @@ class M2XWrapper {
     this.deviceId = config.get('m2x:deviceId')
   }
 
-  postUpdate(values, timestamp) {
-    return new Promise((resolve, reject) => {
-      const params = { values: values }
-      if (timestamp) {
-        params.timestamp = timestamp instanceof Date ? timestamp.toISOString() : timestamp
-      }
-
-      this.m2x.devices.postUpdate(this.deviceId, params, response => {
-        if (response.status === 202) {
-          resolve()
-        } else {
-          reject(response)
-        }
-      })
-    })
-  }
-
   postUpdates(params) {
     return new Promise((resolve, reject) => {
       this.m2x.devices.postUpdates(this.deviceId, params, response => {
